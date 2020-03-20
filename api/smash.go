c3c9493f032957b625fe5149f60513950befc54f
Kit Martin <kit@fossa.com>
2020-03-20 01:59:42 -0500

    Send an interactive response

--- api/smash.go
+++ api/smash.go
@@ -20,10 +20,24 @@ func HandleSmashCommand(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// Create the interactive response that allows other users to join in
 	log.Printf("User '%s' is creating an arena", cmd.UserName)
-	m := &slack.Msg{Text: fmt.Sprintf("%s is creating a Smash Ultimate arena!", cmd.UserName)}
+	headerText := slack.NewTextBlockObject("mrkdwn", fmt.Sprintf("**%s** is creating a new Smash arena!", cmd.UserName), false, false)
+	headerSection := slack.NewSectionBlock(headerText, nil, nil)
+
+	joinBtnText := slack.NewTextBlockObject("plan_text", "Join!", false, false)
+	joinBtn := slack.NewButtonBlockElement("", "join", joinBtnText)
+	actionBlock := slack.NewActionBlock("", joinBtn)
+
+	msg := slack.NewBlockMessage(
+		headerSection,
+		actionBlock,
+	)
+	msg.ResponseType = "in_channel"
+
+	// Send the response
 	w.Header().Set("Content-Type", "application/json")
-	if err := json.NewEncoder(w).Encode(m); err != nil {
+	if err := json.NewEncoder(w).Encode(msg); err != nil {
 		log.Printf("[error] respond to command: %+v", err)
 		return
 	}
